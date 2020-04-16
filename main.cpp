631bb9b2737714b14d8c9e6f9cc155b4f745e7c0
kgt2010 <yale_usa@naver.com>
2020-04-16 03:01:49 -0700

    hw2-2 done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,24 @@
 int sorted[10];
 
 int main(){
+    int i=0;
+    int data[10];
+
+
+    for(i=0; i<10; i++){
+        cin >>data[i];
+    }
+
+    merge_sort(data, 0, 9);
+
+    for(i=0; i<10; i++){
+        cout <<data[i]<<endl ;
+    }
+
+
+
+
+
 
     return 0;
 }
